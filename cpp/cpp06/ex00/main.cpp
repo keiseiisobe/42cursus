9b5609a65a23968dda469d05ec7c60a7170dbb65
keiseiisobe <keisei.isobe@gmail.com>
2024-08-24 18:08:40 +0900

    modified test.sh

--- cpp/cpp06/ex00/main.cpp
+++ cpp/cpp06/ex00/main.cpp
@@ -1,7 +1,14 @@
 #include "ScalarConverter.hpp"
 
+
 int	main(int argc, char **argv)
 {
+//	std::cout << static_cast<float>(std::numeric_limits<int>::max()) << std::endl;
+//	std::cout << static_cast<float>(std::numeric_limits<int>::min()) << std::endl;
+//	std::cout << std::numeric_limits<float>::max() << std::endl;
+//	std::cout << std::numeric_limits<float>::min() << std::endl;
+//	std::cout << std::numeric_limits<double>::max() << std::endl;
+//	std::cout << std::numeric_limits<double>::min() << std::endl;
 	switch (argc)
 	{
 		case 2:
--- cpp/cpp06/ex00/test.sh
+++ cpp/cpp06/ex00/test.sh
@@ -2,8 +2,26 @@
 
 ofile="result.txt";
 
+rm -rf result.txt;
+
 echo "**********char test**********" >> ${ofile};
 arg=('a' 'b' 'c' 'x' 'y' 'z' 'A' 'B' 'C' 'X' 'Y' 'Z' '!' '"' "#" '$' '{' '|' '}' '~');
+
+for i in ${arg[@]}
+do
+	./convert ${i} >> ${ofile}
+done
+
+echo "**********int test**********" >> ${ofile};
+arg=(0 1 -1 100 -100 2147483647 -2147483648 2147483648 -2147483649 9223372036854775807 -9223372036854775808 9223372036854775808 -9223372036854775809);
+
+for i in ${arg[@]}
+do
+	./convert ${i} >> ${ofile}
+done
+
+echo "**********float test**********" >> ${ofile};
+arg=(0.0f 0.1f -0.1f 42.42f -42.42f 2147483647.5f -2147483648.5f 3.40282e+38f 1.17549e-38f 1.79769e+308f 2.22507e-308f nanf +inff -inff);
 
 for i in ${arg[@]}
 do
